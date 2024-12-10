06ed4fd199af00db538446b6176d93e6dafe2f96
Izkwy <leger.joris091104@gmail.com>
2024-12-10 09:56:12 +0100

    valid

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,18 +4,45 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'noir': '#333333',
-				'blanc': '#F6F0E2',
-				'vert': '#22C55E',
-				'rouge': '#EF4444',
-				'jaune': '#FACC15',
-
-		},
-		fontFamily: {
-			'titre': ['Carena', 'sans-serif'],
-			'texte': ['Inria Sans', 'sans-serif'],
-		},
+				noir: '#333333',
+				blanc: '#F6F0E2',
+				vert: '#22C55E',
+				rouge: '#EF4444',
+				jaune: '#FACC15',
+			},
+			fontFamily: {
+				titre: ['Carena', 'sans-serif'],
+				texte: ['Inria Sans', 'sans-serif'],
+			},
 		},
 	},
-	plugins: [],
-}
+	plugins: [
+		function ({ addUtilities }) {
+			addUtilities({
+				'::-webkit-scrollbar': {
+					width: '12px',
+					height: '12px',
+				},
+				'::-webkit-scrollbar-track': {
+					background: '#F6F0E2',
+					'border-radius': '10px',
+				},
+				'::-webkit-scrollbar-thumb': {
+					'background-color': '#333333',
+					'border-radius': '10px',
+					'border': '2px solid #F6F0E2',
+				},
+				'::-webkit-scrollbar-thumb:hover': {
+					'background-color': '#555555',
+				},
+				'html': {
+					'scrollbar-width': 'thin',
+					'scrollbar-color': '#333333 #F6F0E2',
+				},
+				'body': {
+					overflow: 'overlay',
+				},
+			});
+		},
+	],
+};
